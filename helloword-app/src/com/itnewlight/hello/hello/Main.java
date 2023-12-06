6b416a60f3a8f67f31c08811f6ea5451081d7b9f
NewLight <256263487@qq.com>
2023-12-06 18:21:47 +0800

    Add SpeedKey

--- helloword-app/src/com/itnewlight/hello/hello/Main.java
+++ helloword-app/src/com/itnewlight/hello/hello/Main.java
@@ -1,4 +1,4 @@
-package com.itnewlight.hello;
+package com.itnewlight.hello.hello;
 
 public class Main {
     public static void main(String[] args) {
---
+++ helloword-app/src/com/itnewlight/hello/speedkey/SpeedKey.java
@@ -0,0 +1,10 @@
+package com.itnewlight.hello.speedkey;
+
+public class SpeedKey {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World 1");
+        System.out.println("Hello World ");
+        System.out.println("Hello World ");
+    }
+}
